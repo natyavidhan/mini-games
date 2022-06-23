269be8f4c354af14926d235566465377f310d7ae
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-23 16:10:20 +0530

    base code

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((750, 550))
+        pygame.display.set_caption("Game of Life")
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+        
+        self.screen.fill((0, 0, 0))
+
+        self.clock.tick(60)
+        pygame.display.update()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
